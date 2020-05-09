df5a8839e697ee527aa4cbf1e4b3368d5d698588
Przemysław Mirowski <miretpl@gmail.com>
2020-05-09 15:17:26 +0200

    add state created and submitted test

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,9 +1,25 @@
 package edu.iis.mto.time;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class OrderTest {
 
+    private Order order;
+
     @Before public void setUp() throws Exception {
+        order = new Order();
+    }
+
+    @Test public void OrderCreatedState() {
+        assertThat(order.getOrderState(), is(Order.State.CREATED));
+    }
+
+    @Test public void OrderSubmitedState() {
+        order.submit();
+        assertThat(order.getOrderState(), is(Order.State.SUBMITTED));
     }
 }
